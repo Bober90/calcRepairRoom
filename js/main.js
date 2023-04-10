163ae9ed933d83d60bef64fc74e85788dfa38bf3
Trach Volodymyr <vtlviv90@gmail.com>
2023-04-10 13:22:08 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,81 @@
+const squareInput=document.querySelector('#square-input');
+const squareRange=document.querySelector('#square-range');
+const inputs=document.querySelectorAll('input');
+
+//Раідокнопки
+const radioType=document.querySelectorAll('input[name="type"]');
+const radioBuilding=document.querySelectorAll('input[name="building"]');
+const radioRooms=document.querySelectorAll('input[name="rooms"]');
+
+//Чек бокс
+const ceilings=document.querySelector('input[name="ceiling"]');
+const walls=document.querySelector('input[name="walls"]');
+const floor=document.querySelector('input[name="floor"]');
+
+
+const basePrice= 35000;
+const totalPriceElement=document.querySelector('#total-price');
+
+
+
+squareRange.addEventListener('input', function(){
+    squareInput.value=squareRange.value;
+});
+
+
+squareInput.addEventListener('input', function(){
+  
+    squareRange.value=squareInput.value;
+});
+
+
+function calculate(){
+    let totalPrice= basePrice*parseInt(squareInput.value);
+ 
+
+    for(const radio of radioType){
+        if (radio.checked){
+            totalPrice= totalPrice * parseFloat(radio.value);
+        }
+    }
+
+    for(const radio of radioBuilding){
+        if (radio.checked){
+            totalPrice= totalPrice * parseFloat(radio.value);
+        }
+    }
+
+    for(const radio of radioRooms){
+        if (radio.checked){
+            totalPrice= totalPrice * parseFloat(radio.value);
+        }
+    }
+
+    if(ceilings.checked){
+        totalPrice = totalPrice* parseFloat(ceilings.value);
+        console.log(ceilings);
+    }
+
+    if(walls.checked){
+        totalPrice=totalPrice* parseFloat(walls.value);
+    }
+
+    if(floor.checked){
+        totalPrice=totalPrice* parseFloat(floor.value);
+    }
+
+
+    const formatter=new Intl.NumberFormat('ua');
+    
+    totalPriceElement.innerText= formatter.format(totalPrice);
+}
+calculate();
+
+
+
+for (const input of inputs){
+input.addEventListener('input', function(){
+   
+   calculate();
+})
+};
